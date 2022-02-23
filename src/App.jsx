0980d2ba5429dc9f6e4a1800a9499e4c41ef4c62
m-shimizu <62657297+m-shimizu-ring@users.noreply.github.com>
2022-02-23 21:38:41 +0900

    style

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,14 @@ export const App = () => {
   const onClickButton = () => {
     alert("イベントが発火されました。");
   };
+
+  const styleContent = {
+    color: "blue",
+    fontSize: "33px"
+  };
   return (
     <>
-      <h1>こんにちは</h1>
+      <h1 style={styleContent}>こんにちは</h1>
       <p>お元気ですか？</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
