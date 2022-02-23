924b46a0d8b80b290117b6e20f233721c4967d40
m-shimizu <62657297+m-shimizu-ring@users.noreply.github.com>
2022-02-23 21:36:38 +0900

    Clickイベント

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import "./styles.css";
 
 export const App = () => {
+  const onClickButton = () => {
+    alert("イベントが発火されました。");
+  };
   return (
     <>
       <h1>こんにちは</h1>
       <p>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
