5ff4694087be13d18956b978a1c4e21707ef8d27
m-shimizu <62657297+m-shimizu-ring@users.noreply.github.com>
2022-02-24 23:30:37 +0900

    State

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import "./styles.css";
+import React, { useState } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
-  const onClickButton = () => {
-    alert("イベントが発火されました。");
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
+
+  const [num, setNum] = useState(0);
 
   return (
     <>
       <ColorfulMessage color="blue" message="こんにちは" />
-      <p>お元気ですか？</p>
-      <button onClick={onClickButton}>ボタン</button>
+      <p>{num}</p>
+      <button onClick={onClickCountUp}>カウントアップ</button>
     </>
   );
 };
